1602782c2eed58d92dfceb0322d35181759aaa40
Artur <alferovnet@mail.ru>
2022-05-27 16:43:09 +0300

    add func in FullInfoVC

--- BreakingBad/ViewControllers/FullInfoViewController.swift
+++ BreakingBad/ViewControllers/FullInfoViewController.swift
@@ -16,13 +16,20 @@ class FullInfoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configure()
     }
 }
 
 extension FullInfoViewController{
     func configure(){
-        
+        descriptionCharacter.text = character.description
+        DispatchQueue.global().async {
+            guard let url = URL(string: self.character.img ?? "") else {return}
+            guard let imageData = try? Data(contentsOf: url) else {return}
+            DispatchQueue.main.async {
+                self.photoCharacter.image = UIImage(data: imageData)
+            }
+        }
     }
 }
 
