c11fca7207d547bbd9a313c07fb23a4a2892cbe9
Artur <alferovnet@mail.ru>
2022-05-27 19:57:21 +0300

    add func in VC to work with NetworkManager

--- BreakingBad/NetworkManager/NetworkManager.swift
+++ BreakingBad/NetworkManager/NetworkManager.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+import UIKit
+
+enum NetworkError: Error {
+  case badURL
+  case invalidData
+  case decodeError
+}
+
+struct NetworkManager {
+    static let shared = NetworkManager()
+    private let urlString = "https://www.breakingbadapi.com/api/characters"
+    
+    private init() {}
+    
+    func fetchCharacter(completionHandler: @escaping(Result <[Characters], NetworkError>) -> Void){
+        guard let url = URL(string: urlString) else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                completionHandler(.failure(.invalidData))
+                return
+            }
+            
+            do{
+                let character = try JSONDecoder().decode([Characters].self, from: data)
+                completionHandler(.success(character))
+            } catch let error {
+                print(error)
+                completionHandler(.failure(.decodeError))
+            }
+        }.resume()
+    }
+    
+    func fetchImage(with character: Characters ,
+                    completionHandler: @escaping(Result <Data, NetworkError>) -> Void){
+        DispatchQueue.global().async {
+            guard let url = URL(string: character.img ?? "") else {
+                completionHandler(.failure(.badURL))
+                return
+            }
+            if let imageData = try? Data(contentsOf: url){
+                completionHandler(.success(imageData))
+                return
+            }else {
+                completionHandler(.failure(.invalidData))
+                return
+            }
+        }
+    }
+}
