44b9bfc106bd027c0fc91713929133260502e286
Artur <alferovnet@mail.ru>
2022-05-27 15:02:01 +0300

    add custom cell and new VC

---
+++ BreakingBad/Model/Characters.swift
@@ -0,0 +1,15 @@
+//
+//  Characters.swift
+//  BreakingBad
+//
+//  Created by Артур Сахбиев on 27.05.2022.
+//
+
+import Foundation
+
+struct Characters: Decodable {
+    let char_id: Int
+    let name: String
+    let img: String
+    let portrayed: String
+}
